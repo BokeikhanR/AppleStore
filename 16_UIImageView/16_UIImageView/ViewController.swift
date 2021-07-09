be2824f20acbf46cd7e02f6941f84416c4ce6397
Ruslan <Rus.bokeykhan.97@bk.ru>
2021-07-09 15:55:04 +0600

    первый проект

---
+++ 16_UIImageView/16_UIImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16_UIImageView
+//
+//  Created by admin on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

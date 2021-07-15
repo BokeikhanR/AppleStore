34397613877ce5deaabe21f0602459365cb4665d
Ruslan <Rus.bokeykhan.97@bk.ru>
2021-07-15 14:11:45 +0600

    Сделал домашку 19

---
+++ 19_UIImageView — UIScrollView, WKWebView, UIProgressView/16_UIImageView/ ForYouViewController.swift
@@ -0,0 +1,275 @@
+//
+//  SecondViewController.swift
+//  16_UIImageView
+//
+//  Created by admin on 09.07.2021.
+//
+
+import UIKit
+
+final class ForYouViewController: UIViewController {
+
+//MARK: - View Properties
+    
+    private var myScrollView = UIScrollView()
+    private var avatarButton = UIButton()
+    private var addButton = UIButton()
+    private var sozButton = UIButton()
+    private var noviLabel = UILabel()
+    private var novostiView = UIView()
+    private var zakazLabel = UILabel()
+    private var dostavkaLabel = UILabel()
+    private var chevronRightButton = UIButton()
+    private var zakazProgressView = UIProgressView()
+    private var prosesLabel = UILabel()
+    private var sendLabel = UILabel()
+    private var dostkaLabel = UILabel()
+    private var recomendasislabel = UILabel()
+    private var ustroistvalabel = UILabel()
+    private var pokazatVseLabel = UILabel()
+    private var novostiLabel = UILabel()
+    private var uvedLabel = UILabel()
+    private var bellButton = UIButton()
+    private var chevronButton = UIButton()
+    private var uvedomlenieButton = UIButton()
+
+
+
+//MARK: - View Components
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        createSamScrollView()
+        createToBarButtonItem()
+        createView()
+        navigationControll()
+        createZakazView()
+        createNoviLabel()
+        createRecomendationLabel()
+        createNovostiLabel()
+        createUvedomlenieLabel()
+        createChevronAndBellButtons()
+        createUstoistvaLabels()
+
+    }
+
+
+//MARK: - Label Elements
+
+    fileprivate func createNoviLabel() {
+        noviLabel.frame = CGRect(x: 20, y: 80, width: 250, height: 100)
+        noviLabel.text = "Вот что нового"
+        noviLabel.textColor = .black
+        noviLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        noviLabel.textAlignment = .left
+        view.addSubview(noviLabel)
+    }
+
+    fileprivate func createRecomendationLabel() {
+        recomendasislabel.frame = CGRect(x: 20, y: 400,
+                                         width: 250, height: 100)
+        recomendasislabel.text = "Рекомендуется вам"
+        recomendasislabel.textColor = .black
+        recomendasislabel.font = UIFont.boldSystemFont(ofSize: 22)
+        recomendasislabel.textAlignment = .left
+        view.addSubview(recomendasislabel)
+    }
+
+    fileprivate func createNovostiLabel() {
+        novostiLabel.frame = CGRect(x: 120, y: 490,
+                                    width: 200, height: 100)
+        novostiLabel.text = "Получайте новости о своем заказе в режиме реального времени"
+        novostiLabel.textColor = .black
+        novostiLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        novostiLabel.textAlignment = .left
+        novostiLabel.numberOfLines = 0
+        novostiLabel.adjustsFontSizeToFitWidth = true
+        novostiLabel.sizeToFit()
+        view.addSubview(novostiLabel)
+    }
+
+    fileprivate func createUvedomlenieLabel() {
+        uvedLabel.frame = CGRect(x: 120, y: 550,
+                                 width: 230, height: 100)
+        uvedLabel.text = "Включите уведомления, чтобы получать новости о своем заказе."
+        uvedLabel.textColor = .gray
+        uvedLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        uvedLabel.textAlignment = .left
+        uvedLabel.numberOfLines = 0
+        uvedLabel.adjustsFontSizeToFitWidth = true
+        uvedLabel.sizeToFit()
+        view.addSubview(uvedLabel)
+    }
+
+
+//MARK: - View Elements
+
+    fileprivate func createZakazView() {
+        //View Zakaz
+        novostiView.frame = CGRect(x: 20, y: 180, width: 350, height: 150)
+        novostiView.backgroundColor = .white
+        novostiView.layer.shadowColor = UIColor.systemGray.cgColor
+        novostiView.layer.shadowOpacity = 1
+        novostiView.layer.shadowRadius = 5
+        novostiView.layer.cornerRadius = 10
+
+        let airPodsImage = UIImageView(image: UIImage(named: "AirPods"))
+        airPodsImage.frame = CGRect(x: 20, y: 20, width: 80, height: 80)
+        novostiView.addSubview(airPodsImage)
+
+        zakazLabel.frame = CGRect(x: 100, y: 25, width: 200, height: 30)
+        zakazLabel.text = "Ваш заказ отправлен"
+        zakazLabel.textColor = .black
+        novostiView.addSubview(zakazLabel)
+
+        dostavkaLabel.frame = CGRect(x: 100, y: 50, width: 200, height: 30)
+        dostavkaLabel.text = "1 Товар, доставка завтра"
+        dostavkaLabel.textColor = .gray
+        novostiView.addSubview(dostavkaLabel)
+
+        let myView = UIView()
+        myView.frame = CGRect(x: 0, y: 100, width: 350, height: 1)
+        myView.backgroundColor = .systemGray4
+        novostiView.addSubview(myView)
+
+
+        chevronRightButton.frame = CGRect(x: 295, y: 48, width: 50 , height: 50)
+        chevronRightButton.setImage(UIImage(named: "chevron.right-2"), for: .normal)
+        novostiView.addSubview(chevronRightButton)
+
+        //Zakaz Progress View
+        zakazProgressView.progressViewStyle = .default
+        zakazProgressView.frame = CGRect(x: 30, y: 120, width: 300, height: 40)
+        zakazProgressView.setProgress(0.5, animated: true)
+        zakazProgressView.progressTintColor = .systemGreen
+        novostiView.addSubview(zakazProgressView)
+
+        //ZAkaz obrabotka
+
+        prosesLabel.frame = CGRect(x: 10, y: 130, width: 100, height: 20)
+        prosesLabel.text = "Обрабатывается"
+        prosesLabel.font = UIFont(name: "arial", size: 12)
+        novostiView.addSubview(prosesLabel)
+
+        sendLabel.frame = CGRect(x: 140, y: 130, width: 100, height: 20)
+        sendLabel.text = "Отпраавлено"
+        sendLabel.font = UIFont(name: "arial", size: 12)
+        novostiView.addSubview(sendLabel)
+
+        dostkaLabel.frame = CGRect(x: 260, y: 130, width: 100, height: 20)
+        dostkaLabel.text = "Доставлено"
+        dostkaLabel.textColor = .gray
+        dostkaLabel.font = UIFont(name: "arial", size: 12)
+        novostiView.addSubview(dostkaLabel)
+
+        view.addSubview(novostiView)
+    }
+
+    //MARK: - createChevronAndBellButtons
+
+
+    fileprivate func createChevronAndBellButtons() {
+    //buttons
+    let bellImage = UIImage(named: "bell.square")
+    bellButton.frame = CGRect(x: 20, y: 460, width: 100, height: 100)
+    bellButton.imageView?.sizeToFit()
+    bellButton.setImage(bellImage, for: .normal)
+    view.addSubview(bellButton)
+
+    let chevronImage = UIImage(systemName: "chevron.right")
+    chevronButton.frame = CGRect(x: 300, y: 490, width: 80, height: 80)
+    chevronButton.setImage(chevronImage, for: .normal)
+    chevronButton.backgroundColor = .white
+    chevronButton.imageView?.sizeToFit()
+    view.addSubview(chevronButton)
+}
+
+
+//MARK: - createUstoistvaLabels
+    private func createUstoistvaLabels() {
+        //ustoistva labels
+        ustroistvalabel.frame = CGRect(x: 20, y: 600,
+                                       width: 250, height: 100)
+        ustroistvalabel.text = "ваши устройства"
+        ustroistvalabel.textColor = .black
+        ustroistvalabel.font = UIFont.boldSystemFont(ofSize: 26)
+        ustroistvalabel.textAlignment = .left
+        view.addSubview(ustroistvalabel)
+
+        pokazatVseLabel.frame = CGRect(x: 270, y: 605,
+                                       width: 250, height: 100)
+        pokazatVseLabel.text = "Показать все"
+        pokazatVseLabel.textColor = .systemBlue
+        pokazatVseLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        pokazatVseLabel.textAlignment = .left
+        view.addSubview(pokazatVseLabel)
+    }
+
+//MARK: - Navigation Controll
+    private func navigationControll(){
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+//MARK: - Create Scroll View
+
+    fileprivate func createSamScrollView() {
+        myScrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view = myScrollView
+    }
+
+
+//MARK: - Create Bar Button Item
+  private func createToBarButtonItem() {
+    let suret = UIImage(named: "download")?.resizeImage(to: CGSize(width: 30, height: 30))
+    addButton.setImage(suret, for: .normal)
+    addButton.backgroundColor = .systemBlue
+    addButton.layer.cornerRadius = 15
+    addButton.clipsToBounds = true
+    addButton.addTarget(self, action: #selector(addNewPerson), for: .touchUpInside)
+
+    let barButton = UIBarButtonItem()
+    barButton.customView = addButton
+
+    navigationItem.rightBarButtonItem = barButton
+  }
+
+    @objc func addNewPerson(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            let img = image.resizeImage(to: CGSize(width: 30, height: 30))
+            addButton.setImage(img, for: .normal)
+
+            dismiss(animated: true)
+        }
+    }
+
+//MARK: - View Methods
+
+    private func createView() {
+        title = "Для вас"
+        view.backgroundColor = .white
+    }
+}
+
+//MARK: Extension UIImage
+extension UIImage {
+    func resizeImage(to size: CGSize) -> UIImage{
+        return UIGraphicsImageRenderer(size: size).image{_ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
+extension ForYouViewController: UIImagePickerControllerDelegate{
+
+}
+
+extension ForYouViewController: UINavigationControllerDelegate{
+
+}
---
+++ 19_UIImageView — UIScrollView, WKWebView, UIProgressView/16_UIImageView/SearchViewController.swift
@@ -0,0 +1,331 @@
+//
+//  ViewController.swift
+//  16_UIImageView
+//
+//  Created by admin on 09.07.2021.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+//MARK: - Private properties
+
+    private var tabBarImage = UITabBarItem()
+    private var searchLabel = UILabel()
+    private var prosmotrLabel = UILabel()
+    private var ochistitLabel = UILabel()
+    private var variantLabel = UILabel()
+
+    private var poiskTextField = UITextField()
+    private var airPodsTextField = UITextField()
+    private var appleCareTextField = UITextField()
+    private var beatsTextField = UITextField()
+    private var iphoneTextField = UITextField()
+
+    private var inhaceFlatView = UIView()
+    private var nameElementLabel = UILabel()
+    private var inhaceFlatImage = UIImageView()
+
+    private var blackUnityView = UIView()
+    private var caseBownView = UIView()
+    private var nameElementLabel2 = UILabel()
+    private var nameElementLabel3 = UILabel()
+    private var blackUnityImageView = UIImageView()
+    private var caseBownImageView = UIImageView()
+
+    private var elementsScrollView = UIScrollView()
+
+//MARK: - View Components
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollViewFunction()
+
+        tapToGisture()
+        viewElementChehol()
+        viewElementBelt()
+        createPoiskTextField()
+        createZaprosTextField()
+        elementLabels()
+        viewElementFunc()
+        caseBownFunction()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.isNavigationBarHidden = false
+    }
+
+
+//MARK: - Scroll View
+
+    private func scrollViewFunction() {
+
+        elementsScrollView.addSubview(inhaceFlatView)
+        elementsScrollView.addSubview(blackUnityView)
+        elementsScrollView.addSubview(caseBownImageView)
+        let scrollViewRect = CGRect(x: 20, y: 250, width: 350, height: 200)
+        elementsScrollView.frame = scrollViewRect
+        elementsScrollView.isPagingEnabled = true
+        elementsScrollView.contentSize = CGSize(width: scrollViewRect.size.width * 3,
+                                                height: scrollViewRect.size.height)
+        view.addSubview(elementsScrollView)
+    }
+
+//MARK: - View Component Methods
+
+//View Elements
+
+    private func viewElementFunc() {
+        title = "Поиск"
+        view.backgroundColor = .black
+    }
+
+//Element Labels
+
+    private func elementLabels() {
+        searchLabel.frame = CGRect(x: 20, y: 50, width: 200, height: 60)
+        searchLabel.text = "Поиск"
+        searchLabel.textColor = .systemGray6
+        searchLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        view.addSubview(searchLabel)
+
+        prosmotrLabel.frame = CGRect(x: 20, y: 200, width: 300, height: 37)
+        prosmotrLabel.text = "Недавно просмотренные"
+        prosmotrLabel.textColor = .systemGray6
+        prosmotrLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        view.addSubview(prosmotrLabel)
+
+        ochistitLabel.frame = CGRect(x: 310, y: 202, width: 300, height: 40)
+        ochistitLabel.text = "Очистить "
+        ochistitLabel.textColor = .systemBlue
+        ochistitLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        view.addSubview(ochistitLabel)
+
+        variantLabel.frame = CGRect(x: 20, y: 442, width: 300, height: 40)
+        variantLabel.text = "Варианты запросов"
+        variantLabel.textColor = .systemGray6
+        variantLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        view.addSubview(variantLabel)
+    }
+
+//MARK: - Zapros TextFields
+
+    private func createZaprosTextField() {
+
+        airPodsTextField.frame = CGRect(x: 25, y: 480, width: 200, height: 40)
+        let poiskImage1 = UIImage(named: "magnifyingglass")
+        addLeftImageTo1(txtField: airPodsTextField, andImage: poiskImage1!)
+        airPodsTextField.text = "   AirPods"
+        airPodsTextField.textColor = .systemGray6
+        airPodsTextField.font = UIFont.boldSystemFont(ofSize: 20)
+        airPodsTextField.backgroundColor = .black
+        airPodsTextField.layer.cornerRadius = 10
+        view.addSubview(airPodsTextField)
+
+        // appleCareTextField number 2
+
+        appleCareTextField.frame = CGRect(x: 25, y: 530, width: 200, height: 40)
+        let poiskImage2 = UIImage(named: "magnifyingglass")
+        addLeftImageTo2(txtField: appleCareTextField, andImage: poiskImage2!)
+        appleCareTextField.text = "   AppleCare"
+        appleCareTextField.textColor = .systemGray6
+        appleCareTextField.font = UIFont.boldSystemFont(ofSize: 20)
+        appleCareTextField.backgroundColor = .black
+        appleCareTextField.layer.cornerRadius = 10
+        view.addSubview(appleCareTextField)
+
+        // beatsTextField number 3
+
+        beatsTextField.frame = CGRect(x: 25, y: 580, width: 200, height: 40)
+        let poiskImage3 = UIImage(named: "magnifyingglass")
+        addLeftImageTo3(txtField: beatsTextField, andImage: poiskImage3!)
+        beatsTextField.text = "   Beats"
+        beatsTextField.textColor = .systemGray6
+        beatsTextField.font = UIFont.boldSystemFont(ofSize: 20)
+        beatsTextField.backgroundColor = .black
+        beatsTextField.layer.cornerRadius = 10
+        view.addSubview(beatsTextField)
+
+        // iphoneTextField number 4
+
+        iphoneTextField.frame = CGRect(x: 25, y: 630, width: 300, height: 40)
+        let poiskImage4 = UIImage(named: "magnifyingglass")
+        addLeftImageTo4(txtField: iphoneTextField, andImage: poiskImage4!)
+        iphoneTextField.text = "   Сравните модели Iphone"
+        iphoneTextField.textColor = .systemGray6
+        iphoneTextField.font = UIFont.boldSystemFont(ofSize: 20)
+        iphoneTextField.backgroundColor = .black
+        iphoneTextField.layer.cornerRadius = 10
+        view.addSubview(iphoneTextField)
+    }
+
+//MARK: - View Elements
+
+    //caseBown View
+    private func caseBownFunction() {
+        caseBownView.frame = CGRect(x: 360, y: 10, width: 150, height: 180)
+        nameElementLabel3.frame = CGRect(x: 20, y: 130, width: 120, height: 30)
+        caseBownImageView.frame = CGRect(x: 10, y: 11, width: 120, height: 100)
+        caseBownImageView.image = UIImage(named: "caseBown")
+        caseBownImageView.contentMode = .scaleAspectFit
+        nameElementLabel3.text = "Кожанный чехол для MacBook, 16 дьюмовый"
+        nameElementLabel3.textColor = .white
+        nameElementLabel3.font = UIFont.boldSystemFont(ofSize: 10)
+        nameElementLabel3.numberOfLines = 0
+        nameElementLabel3.adjustsFontSizeToFitWidth = true
+        nameElementLabel3.sizeToFit()
+        caseBownView.addSubview(caseBownImageView)
+        caseBownView.addSubview(nameElementLabel3)
+        caseBownView.backgroundColor = .systemGray
+        caseBownView.layer.cornerRadius = 10
+        caseBownView.tag = 2
+        elementsScrollView.addSubview(caseBownView)
+    }
+
+    //blackUnity View
+    private func viewElementBelt() {
+
+        blackUnityView.frame = CGRect(x: 190, y: 10, width: 150, height: 180)
+        nameElementLabel2.frame = CGRect(x: 20, y: 130, width: 120, height: 30)
+        blackUnityImageView.frame = CGRect(x: 10, y: 11, width: 120, height: 100)
+        blackUnityImageView.image = UIImage(named: "belt")
+        blackUnityImageView.contentMode = .scaleAspectFit
+        nameElementLabel2.text = "Спортивный ремешок Black Unity"
+        nameElementLabel2.textColor = .white
+        nameElementLabel2.font = UIFont.boldSystemFont(ofSize: 10)
+        nameElementLabel2.numberOfLines = 0
+        nameElementLabel2.adjustsFontSizeToFitWidth = true
+        nameElementLabel2.sizeToFit()
+        blackUnityView.addSubview(blackUnityImageView)
+        blackUnityView.addSubview(nameElementLabel2)
+        blackUnityView.backgroundColor = .systemGray
+        blackUnityView.layer.cornerRadius = 10
+        blackUnityView.tag = 1
+        elementsScrollView.addSubview(blackUnityView)
+    }
+
+    //inhaceFlat View
+    private func viewElementChehol() {
+
+        inhaceFlatView.frame = CGRect(x: 20, y: 10, width: 150, height: 180)
+        nameElementLabel.frame = CGRect(x: 20, y: 130, width: 120, height: 30)
+        inhaceFlatImage.frame = CGRect(x: 10, y: 10, width: 120, height: 120)
+        inhaceFlatImage.image = UIImage(named: "case")
+        inhaceFlatImage.contentMode = .scaleAspectFit
+        nameElementLabel.text = "Чехол Inhace Flat для MacBook Pro 16 дюймов"
+        nameElementLabel.textColor = .white
+        nameElementLabel.font = UIFont.boldSystemFont(ofSize: 10)
+        nameElementLabel.numberOfLines = 0
+        nameElementLabel.adjustsFontSizeToFitWidth = true
+        nameElementLabel.sizeToFit()
+        inhaceFlatView.addSubview(inhaceFlatImage)
+        inhaceFlatView.addSubview(nameElementLabel)
+        inhaceFlatView.backgroundColor = .systemGray
+        inhaceFlatView.layer.cornerRadius = 10
+        inhaceFlatView.tag = 0
+        elementsScrollView.addSubview(inhaceFlatView)
+    }
+
+//Text Field
+
+    private func createPoiskTextField(){
+
+        poiskTextField.frame = CGRect(x: 20, y: 120, width: 350, height: 40)
+        let poiskImage = UIImage(named: "magnifyingglass")
+        addLeftImageTo(txtField: poiskTextField, andImage: poiskImage!)
+        poiskTextField.text = " Поиск по продуктам и магазинам"
+        poiskTextField.textColor = .systemGray6
+        poiskTextField.backgroundColor = .systemGray
+        poiskTextField.layer.cornerRadius = 10
+        view.addSubview(poiskTextField)
+    }
+
+//MARK: - Gesture Functions
+
+    private func tapToGisture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hundleTap))
+        inhaceFlatView.addGestureRecognizer(tap)
+        inhaceFlatView.isUserInteractionEnabled = true
+
+        let tapSecond = UITapGestureRecognizer(target: self, action: #selector(hundleTap))
+        blackUnityView.addGestureRecognizer(tapSecond)
+        blackUnityView.isUserInteractionEnabled = true
+
+        let tapThird = UITapGestureRecognizer(target: self, action: #selector(hundleTap))
+        caseBownView.addGestureRecognizer(tapThird)
+        caseBownView.isUserInteractionEnabled = true
+    }
+
+    @objc func hundleTap(sender: UITapGestureRecognizer, paramButton: UIView) {
+        switch sender.view {
+        case inhaceFlatView:
+            let productVC = ProductViewController()
+            navigationController?.pushViewController(productVC, animated: true)
+            productVC.labelText = nameElementLabel.text
+            productVC.elementViewImage = inhaceFlatImage.image
+            productVC.senaText = "3 999.0 Тенге"
+        case blackUnityView:
+            let productVC = ProductViewController()
+            navigationController?.pushViewController(productVC, animated: true)
+            productVC.labelText = nameElementLabel.text
+            productVC.elementViewImage = blackUnityImageView.image
+            productVC.senaText = "5 999.0 Тенге"
+        case caseBownView:
+            let productVC = ProductViewController()
+            navigationController?.pushViewController(productVC, animated: true)
+            productVC.labelText = nameElementLabel.text
+            productVC.elementViewImage = caseBownImageView.image
+            productVC.senaText = "6 999.0 Тенге"
+        default:
+            break
+        }
+    }
+
+//MARK: - OBJC Methods
+
+    @objc func addLeftImageTo(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        poiskTextField.leftView = leftImageView
+        poiskTextField.leftViewMode = .always
+    }
+
+    @objc func addLeftImageTo1(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        airPodsTextField.leftView = leftImageView
+        airPodsTextField.leftViewMode = .always
+    }
+
+    @objc func addLeftImageTo2(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        appleCareTextField.leftView = leftImageView
+        appleCareTextField.leftViewMode = .always
+    }
+
+    @objc func addLeftImageTo3(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        beatsTextField.leftView = leftImageView
+        beatsTextField.leftViewMode = .always
+    }
+
+    @objc func addLeftImageTo4(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        iphoneTextField.leftView = leftImageView
+        iphoneTextField.leftViewMode = .always
+    }
+
+}
+
